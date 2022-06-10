65f141ca292fc55f7f0fdcf7f13d7779187f72f5
camil <camillegalicia00@gmail.com>
2022-06-10 20:50:30 +0800

    Added 1st button - Start, to show message box

--- chimmyContactTracing/Form1.Designer.cs
+++ chimmyContactTracing/Form1.Designer.cs
@@ -29,7 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Snow;
+            this.btnStart.Font = new System.Drawing.Font("Mont Blanc", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.ForeColor = System.Drawing.Color.Purple;
+            this.btnStart.Location = new System.Drawing.Point(37, 308);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(208, 47);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // Home
             // 
@@ -37,6 +51,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::chimmyContactTracing.Properties.Resources.ChimmyCT_Home_Background;
             this.ClientSize = new System.Drawing.Size(601, 496);
+            this.Controls.Add(this.btnStart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -48,5 +63,7 @@
         }
 
         #endregion
+
+        private Button btnStart;
     }
 }
--- chimmyContactTracing/Form1.cs
+++ chimmyContactTracing/Form1.cs
@@ -6,5 +6,19 @@ namespace chimmyContactTracing
         {
             InitializeComponent();
         }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show("By clicking 'OK' you agree to our privacy policy.", "Chimmy - Contact Tracing",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("An error occured.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
