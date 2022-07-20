6c1b5fb77f9781661a19fa0ac299343565539de8
camil <camillegalicia00@gmail.com>
2022-07-20 09:29:22 +0800

    Added 1st listbox and 5th button - View All Record, to list and show the record in txt file

--- chimmyContactTracing/Search.Designer.cs
+++ chimmyContactTracing/Search.Designer.cs
@@ -29,7 +29,31 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Search));
+            this.listBoxContactData = new System.Windows.Forms.ListBox();
+            this.btnViewAllRecord = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBoxContactData
+            // 
+            this.listBoxContactData.FormattingEnabled = true;
+            this.listBoxContactData.ItemHeight = 15;
+            this.listBoxContactData.Location = new System.Drawing.Point(24, 130);
+            this.listBoxContactData.Name = "listBoxContactData";
+            this.listBoxContactData.Size = new System.Drawing.Size(553, 319);
+            this.listBoxContactData.TabIndex = 50;
+            // 
+            // btnViewAllRecord
+            // 
+            this.btnViewAllRecord.BackColor = System.Drawing.Color.Snow;
+            this.btnViewAllRecord.Font = new System.Drawing.Font("Mont Blanc SemiBold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnViewAllRecord.ForeColor = System.Drawing.Color.Purple;
+            this.btnViewAllRecord.Location = new System.Drawing.Point(445, 95);
+            this.btnViewAllRecord.Name = "btnViewAllRecord";
+            this.btnViewAllRecord.Size = new System.Drawing.Size(132, 25);
+            this.btnViewAllRecord.TabIndex = 51;
+            this.btnViewAllRecord.Text = "View All Record";
+            this.btnViewAllRecord.UseVisualStyleBackColor = false;
+            this.btnViewAllRecord.Click += new System.EventHandler(this.btnViewAllRecord_Click);
             // 
             // Search
             // 
@@ -37,6 +61,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(601, 500);
+            this.Controls.Add(this.btnViewAllRecord);
+            this.Controls.Add(this.listBoxContactData);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -48,5 +74,8 @@
         }
 
         #endregion
+
+        private ListBox listBoxContactData;
+        private Button btnViewAllRecord;
     }
 }
--- chimmyContactTracing/Search.cs
+++ chimmyContactTracing/Search.cs
@@ -16,5 +16,31 @@ namespace chimmyContactTracing
         {
             InitializeComponent();
         }
+
+        private void btnViewAllRecord_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBoxContactData.Items.Clear();
+
+                string viewAllRecord = @"C:\Users\camil\source\repos\chimmyContactTracing\chimmyContactTracing\Chimmy-ContactTracing-Information.txt";
+                
+                List<string> showRecordsList = new List<string>();
+                showRecordsList = File.ReadAllLines(viewAllRecord).ToList();
+
+                foreach (string line in showRecordsList)
+                {
+                    listBoxContactData.Items.Add(line);
+                    {
+                        File.WriteAllLines(viewAllRecord, showRecordsList.ToArray());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("An error occured.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
