d185493cfdd9ff60d54cb9d89574d59ea8ae7285
camil <camillegalicia00@gmail.com>
2022-07-30 19:05:35 +0800

    Added 24th label - Device, and 2nd combo box in Form 4: Scan, as well as installed packages

--- chimmyContactTracing/Scan.Designer.cs
+++ chimmyContactTracing/Scan.Designer.cs
@@ -29,7 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Scan));
+            this.cmbBoxDevice = new System.Windows.Forms.ComboBox();
+            this.lblDevice = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // cmbBoxDevice
+            // 
+            this.cmbBoxDevice.FormattingEnabled = true;
+            this.cmbBoxDevice.Location = new System.Drawing.Point(89, 107);
+            this.cmbBoxDevice.Name = "cmbBoxDevice";
+            this.cmbBoxDevice.Size = new System.Drawing.Size(229, 23);
+            this.cmbBoxDevice.TabIndex = 0;
+            // 
+            // lblDevice
+            // 
+            this.lblDevice.AutoSize = true;
+            this.lblDevice.BackColor = System.Drawing.Color.Transparent;
+            this.lblDevice.Font = new System.Drawing.Font("Tw Cen MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDevice.ForeColor = System.Drawing.Color.White;
+            this.lblDevice.Location = new System.Drawing.Point(23, 108);
+            this.lblDevice.Name = "lblDevice";
+            this.lblDevice.Size = new System.Drawing.Size(52, 17);
+            this.lblDevice.TabIndex = 61;
+            this.lblDevice.Text = "Device:";
             // 
             // Scan
             // 
@@ -37,14 +59,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDevice);
+            this.Controls.Add(this.cmbBoxDevice);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Scan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chimmy - Contact Tracing";
+            this.Load += new System.EventHandler(this.Scan_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ComboBox cmbBoxDevice;
+        private Label lblDevice;
     }
 }
--- chimmyContactTracing/Scan.cs
+++ chimmyContactTracing/Scan.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace chimmyContactTracing
 {
@@ -15,6 +17,17 @@ namespace chimmyContactTracing
         public Scan()
         {
             InitializeComponent();
+        }
+
+        FilterInfoCollection filterInformationCollection;
+        VideoCaptureDevice scannerDevice;
+
+        private void Scan_Load(object sender, EventArgs e)
+        {
+            filterInformationCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterINfo in filterInformationCollection)
+                cmbBoxDevice.Items.Add(filterINfo.Name);
+            cmbBoxDevice.SelectedIndex = 0; 
         }
     }
 }
